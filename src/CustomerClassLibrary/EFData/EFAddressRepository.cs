aedc03487b3596a6e89601e724854d5a5945b999
Elena <ellevanova@gmail.com>
2021-07-06 00:20:01 +0400

    added addressRepository on EF

--- src/CustomerClassLibrary/EFData/EFAddressRepository.cs
+++ src/CustomerClassLibrary/EFData/EFAddressRepository.cs
@@ -1,4 +1,5 @@
-﻿using CustomerClassLibrary.Repositories;
+﻿using CustomerClassLibrary.Common;
+using CustomerClassLibrary.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,36 +8,81 @@ using System.Threading.Tasks;
 
 namespace CustomerClassLibrary.EFData
 {
-    class EFAddressRepository : IAddressRepository
+    public class EFAddressRepository : IAddressRepository
     {
+        private EFData.CustomerLibraryContext _context;
+
+        public EFAddressRepository()
+        {
+            _context = new CustomerLibraryContext();
+        }
         public int Create(Address address, int idCustomer)
         {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteAllByCustomer(int idCustomer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteByIdAddress(int idAddress)
-        {
-            throw new NotImplementedException();
+            address.IdCustomer = idCustomer;
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+            return address.IdAddress;
         }
 
         public Address ReadByIdAddress(int idAddress)
         {
-            throw new NotImplementedException();
+            var address = _context.Addresses.Find(idAddress);
+            return address;
         }
 
         public List<Address> ReadByIdCustomer(int idCustomer)
         {
-            throw new NotImplementedException();
+            var addresses = _context.Addresses
+                .Where(adr=>adr.IdCustomer==idCustomer)
+                .ToList();
+            return addresses;
         }
 
-        public void Update(Address address, int idCustomer)
+        public void DeleteAllByCustomer(int idCustomer)
         {
-            throw new NotImplementedException();
+            var list = ReadByIdCustomer(idCustomer);
+            _context.Addresses.RemoveRange(_context.Addresses.Where(x => x.IdCustomer == idCustomer));
+            _context.SaveChanges();
+        }
+
+        public void DeleteByIdAddress(int idAddress)
+        {
+            Address address = ReadByIdAddress(idAddress);
+            if (address == null) throw new NotFoundAddressWithId("Not found address with id= " + idAddress);
+            else this.Delete(address);
+        }
+
+        private void Delete(Address address)
+        {
+            try
+            {
+                _context.Addresses.Remove(address);
+                _context.SaveChanges();
+            }
+            catch
+            {
+                throw new NotFoundAddressWithId("Not found address for delete with id=" + address.IdAddress);
+            }
+        }
+
+        public void Update(Address addressNew, int idCustomer)
+        {
+            Address addressOld = _context.Addresses
+                .Where(c => c.IdAddress == addressNew.IdAddress)
+                .FirstOrDefault();
+            if (addressOld != null)
+            {
+                addressOld.AddressLine = addressNew.AddressLine;
+                addressOld.AddressLine2 = addressNew.AddressLine2;
+                addressOld.City = addressNew.City;
+                addressOld.Country = addressNew.Country;
+                addressOld.IdCustomer = addressNew.IdCustomer;
+                addressOld.PostalCode = addressNew.PostalCode;
+                addressOld.State = addressNew.State;
+                addressOld.TypeAddressEnum = addressNew.TypeAddressEnum;
+            }
+
+            _context.SaveChanges();
         }
     }
 }
